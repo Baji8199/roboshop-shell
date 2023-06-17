edeb9f6aaa8b227dc3c0bc96ecd46758cddf035e
nani <nani@gmail.com>
2023-06-17 13:48:59 -0400

    roboshop Nginx Script

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 echo -e "\e[33m Copying the mongodb repo file\e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 echo -e "\e[33m Installing mongodb server\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 # Need to modify the config file
